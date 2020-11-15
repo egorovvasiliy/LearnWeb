4b475b55edd9a9d381c1d4e17990b4799f8a377b
Василий Егоров <polevoy38@mail.ru>
2020-11-15 14:22:06 +0300

    буфер  , коммент

--- AutoZdRoutes.WEB/Services/ChatWS/ChatService.cs
+++ AutoZdRoutes.WEB/Services/ChatWS/ChatService.cs
@@ -111,29 +111,5 @@ namespace AutoZdRoutes.WEB.Services
                 con.Socket.SendAsync(new ArraySegment<byte>(bytesResponse, 0, bytesResponse.Length), WebSocketMessageType.Text, true, CancellationToken.None);
             }
         }
-       //public async Task SendMessageAsync(WebSocket socket, string message)
-       //{
-       //    if (socket.State != WebSocketState.Open)
-       //        return;
-       //    await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(message),
-       //                                                          offset: 0,
-       //                                                          count: message.Length),
-       //                           messageType: WebSocketMessageType.Text,
-       //                           endOfMessage: true,
-       //                           cancellationToken: CancellationToken.None);
-       //}
-       //public async Task SendMessageToAllAsync(string message)
-       //{
-       //    foreach (var pair in socketsList)
-       //    {
-       //        if (pair.Value.State == WebSocketState.Open)
-       //            await SendMessageAsync(pair.Value, message);
-       //    }
-       //}
-       //public async Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
-       //{
-       //    string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-       //    await SendMessageToAllAsync(message);
-       //}
     }
 }
--- AutoZdRoutes.WEB/Startup.cs
+++ AutoZdRoutes.WEB/Startup.cs
@@ -56,7 +56,7 @@ namespace webPortal
             var webSocketOptions = new WebSocketOptions()
             {
                 KeepAliveInterval = TimeSpan.FromSeconds(120),
-                ReceiveBufferSize = 4 * 1024
+                ReceiveBufferSize = 1024,
             };
             app.UseWebSockets(webSocketOptions);
             app.UseMiddleware<ChatMiddleware>();
